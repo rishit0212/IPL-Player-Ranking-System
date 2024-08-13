ae9a734ad573edead8a7d0668a48c3df39961400
Rishit Sharma <115824803+rishit0212@users.noreply.github.com>
2024-08-13 15:09:08 +0530

    Create Perf_Tables.sql

---
+++ Perf_Tables.sql
@@ -0,0 +1,25 @@
+create table Player_Performance( Opp_teamID varchar(5)  , P_num int,  Runs int, Wickets int, catches int );
+-- RCB Players
+INSERT INTO Player_Performance VALUES ('RCB', 1018 , 205, 2, 4);
+INSERT INTO Player_Performance VALUES ('RCB', 1032,  100, 3, 0);
+INSERT INTO Player_Performance VALUES ('RCB', 1073,  40, 6, 1);
+
+-- MI Players
+INSERT INTO Player_Performance VALUES ('MI', 1045,  200, 0, 3);
+INSERT INTO Player_Performance VALUES ('MI', 1063, 110, 1, 1);
+INSERT INTO Player_Performance VALUES ('MI', 1093,  25, 7, 1);
+
+-- DC Players
+INSERT INTO Player_Performance VALUES ('DC', 1030,  145, 1, 2);
+INSERT INTO Player_Performance VALUES ('DC', 1017,  105, 2, 2);
+INSERT INTO Player_Performance VALUES ('DC', 1002,  35, 4, 1);
+
+-- KKR Players
+INSERT INTO Player_Performance VALUES ('KKR', 1041,  160, 0, 2);
+INSERT INTO Player_Performance VALUES ('KKR', 1074,  60, 3, 2);
+INSERT INTO Player_Performance VALUES ('KKR', 1027,  100, 0, 1);
+
+-- CSK Players
+INSERT INTO Player_Performance VALUES ('CSK', 1007,  120, 2, 2);
+INSERT INTO Player_Performance VALUES ('CSK', 1031,  110, 2, 1);
+INSERT INTO Player_Performance VALUES ('CSK', 1008,  75, 3, 1);
